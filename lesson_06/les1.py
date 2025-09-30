6fe5582c7822838e3ab412127c55003c172311ea
IrinaZh81 <irinazh@tutamail.com>
2025-09-30 17:24:53 +0300

    123

--- lesson_06/les1.py
+++ lesson_06/les1.py
@@ -7,14 +7,14 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get("http://uitestingplayground.com/ajax")
 
-    
+
 blue_button = driver.find_element(By.ID, "ajaxButton")
 blue_button.click()
-green_banner = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "bg-success"))
-    )
-    
+
+green_banner = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "bg-success")))
+
 ajax_text = green_banner.text
+
 print(ajax_text)
-    
+
 driver.quit()
